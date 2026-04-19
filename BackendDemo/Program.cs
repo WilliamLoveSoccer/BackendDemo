758cd150ac430feb9bda7905fa8cc8ad8920ba02
WilliamLoveSoccer <william1277903044@gmail.com>
2026-04-18 23:06:55 -0400

    Refactored folder structure

--- BackendDemo/Program.cs
+++ BackendDemo/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IAppDbContext>(sp => sp.GetRequiredService<AppDbContext>());
 
 builder.Services.AddScoped<IAlertRepository, AlertRepository>();
 builder.Services.AddScoped<IGroupRepository, GroupRepository>();
